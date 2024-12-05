55818b89956cec5447611d457e95900050fe5ff5
Saanavi <saanavi31@gmail.com>
2024-12-04 19:23:17 -0500

    testing

---
+++ backend/project/roles.py
@@ -0,0 +1,29 @@
+#declare different roles here to be called later
+
+#user types
+USER = 'U'
+SUPERUSER = 'S'
+VISITOR = 'V'
+VIP = 'VIP'
+user_types = [USER, SUPERUSER, VISITOR, VIP]
+
+#availability
+SOLD = 'S'
+RENTED = 'R'
+FOR_SALE = 'FS'
+FOR_RENT = 'FR'
+availability = [SOLD, RENTED, FOR_SALE, FOR_RENT]
+
+#bid status
+PENDING = 'P'
+ACCEPTED = 'A'
+REJECTED = 'R'
+bid_status = [PENDING, ACCEPTED, REJECTED]
+
+#transaction status
+PENDING = 'P'
+COMPLETED = 'C'
+REJECTED = 'R'
+transaction_status = [PENDING, COMPLETED, REJECTED]
+
+#
